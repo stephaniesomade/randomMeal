b5a9da0537fd748021ca230d20008633fd478d2a
Stephanie <stephaniesomadee@gmail.com>
2022-04-29 20:44:54 +0100

    Added breakfast options

---
+++ src/breakfast.js
@@ -0,0 +1,13 @@
+'use strict'; 
+
+class Breakfast { 
+  constructor() { 
+    this.options =["Fruit and Fibre", "Coco Pops", "Crunchy Nut", "Golden Nuggets", "Toast", "Croissant and Tea", "Greek Yogurt and Apple chunks", "Full English Breakfast", "Pancakes", "Avacado Toast", "French Toast", "Oatmeal and fruits", "Scrambled Eggs", "Eggs Benedict", "Fruit Smoothie", "Omelette", "Granola Bars"]
+  }
+  all() { 
+    return this.options
+  }
+  random() { 
+    return this.options.sort( () => Math.random() - 0.5 )[0]
+  }
+}
