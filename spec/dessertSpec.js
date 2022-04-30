841d59d3609f0b2336ef62748f84e9f058a50ef5
Stephanie <stephaniesomadee@gmail.com>
2022-04-30 19:26:36 +0100

    Dessert options include calories

--- spec/dessertSpec.js
+++ spec/dessertSpec.js
@@ -11,4 +11,8 @@ describe('Dessert', () => {
   it('has a list of desserts', () => { 
     expect(dessert.all()).toEqual(["Chocolate cake", "Jelly", "Cheesecake", "Apple Pie", "Lemon Meringue pie", "Mousse", "Ice-Cream", "Caramel Cake"])
   });
+
+  it('returns the number of calories in a cake', () =>{
+    expect(dessert.calories("Chocolate cake")).toEqual(350)
+  });
 });
--- src/dessert.js
+++ src/dessert.js
@@ -1,12 +1,31 @@
 'use strict';
 class Dessert {
   constructor() { 
-    this.options = ["Chocolate cake", "Jelly", "Cheesecake", "Apple Pie", "Lemon Meringue pie", "Mousse", "Ice-Cream", "Caramel Cake"]
+    this.options = {"Chocolate cake": 350, "Jelly": 296, "Cheesecake": 300, "Apple Pie": 250, "Lemon Meringue pie": 290, "Mousse": 225, "Ice-Cream": 205, "Caramel Cake": 305 }
   }
-  all(){ 
-    return this.options
+    all(){ 
+    let arr = []
+    for (const key in this.options) { 
+      arr.push(`${key}`)
+    }
+    return arr
   }
-  random() { 
-    return this.options.sort( () => Math.random() - 0.5 )[0]
+  random() {
+    let arr = []
+    for (const dessert in this.options) { 
+      arr.push(`${dessert}`)
+    }
+    return arr.sort( () => Math.random() - 0.5 )[0]
+  }
+  calories(item) { 
+    let cal;
+    for (const dessert in this.options) { 
+      if (item === dessert){
+      return this.options[dessert]
+      }
+    }
   }
 }
+
+let dessert = new Dessert();
+console.log(dessert.calories("jelly"));
